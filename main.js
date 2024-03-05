921e2ed4e0c514c352fc1d3bd65bc38eda356336
awannoman <160035058+awannoman@users.noreply.github.com>
2024-03-05 09:38:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Day1
+// question2
+let Name = "Noman";
+console.log('Hello', Name, "would you like to learn some typescript Today?");
+// question3
+let Name = "Noman";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Day1
+// question2
+let Name: string = "Noman";
+console.log('Hello', Name , "would you like to learn some typescript Today?");
+
+// question3
+let Name: string = "Noman";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
